2f7f4b0f7ae67c42a6f3cef32cdd675f872f06ad
mauroking20 <vanroosbroeckmauro@gmail.com>
2025-08-07 08:42:21 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,29 @@
+let Tool = 0
+input.onGesture(Gesture.Shake, function () {
+    Tool = randint(0, 2)
+    if (Tool == 0) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (Tool == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
